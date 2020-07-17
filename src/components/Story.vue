0e373f0d3fe8481c0ce9b76d59882fbc7de08b32
anirbanrkmission <anirbanrkmission@gmail.com>
2020-07-17 23:46:36 +0530

    Story section added

---
+++ src/components/Story.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-container>
+    <h2>{{userData.name}}'s Story</h2>
+    <v-card
+      class="ma-5 pa-5"
+      elevation="3"
+      v-for="feed in reversedFeeds"
+      :key="feed.post.time"
+      tile
+    >
+      <v-card-title>{{feed.post.by}}'s Post</v-card-title>
+      <v-card-subtitle>Posted on: {{getModifiedTime(feed.post.time)}}</v-card-subtitle>
+      <v-card-text>{{feed.post.text}}</v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import store from "../store";
+import axios from "axios";
+
+export default {
+  name: "Story",
+  props: ["userData"],
+  data() {
+    return {
+      feeds: [],
+      postText: ""
+    };
+  },
+  computed: {
+    reversedFeeds() {
+      return (this.feeds != [])? this.feeds.slice().reverse() : [];
+    }
+  },
+  methods: {
+    getModifiedTime(time) {
+      var date = new Date(time);
+      return date.toString();
+    },
+    getPosts() {
+      axios
+        .get("http://localhost:8000/getFeedsOfFriends/" + store.state.id)
+        .then(response => {
+          this.feeds = response.data;
+          var posts = []
+
+          for (var ix in this.feeds) {
+              for (let post of this.feeds[ix].posts) {
+                  posts.push(post)
+              }
+          }
+
+          posts = posts.flat()
+          this.feeds=posts
+          console.log('Stories', this.feeds);
+        });
+    }
+  },
+  mounted() {
+      this.getPosts()
+  }
+};
+</script>
